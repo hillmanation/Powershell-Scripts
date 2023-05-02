ca1d7c77c9bc352666b88b988630ed661957330f
Hillman, Jacob <jake.hillman.it@gmail.com>
2023-05-02 16:05:51 -0400

    New Scripts

---
+++ MultiThreaded-Drive-Audit.ps1
@@ -0,0 +1,84 @@
+﻿$rootfolder = '\\path\to\searchdirectory'
+$logfolder = '\\path\to\log\folder'
+$date = Get-Date
+$jobs = @()
+$MaxThreads = 8
+
+Function MultiThread-Search {
+    Param($directory, $log)
+    $folder = $directory.Name
+
+    $job = Start-Job -ScriptBlock {
+        gci $using:directory.FullName -Recurse |
+        where { (New-TimeSpan -start $_.LastWriteTime -end $using:date).Days -gt 60 -and (New-TimeSpan -start $_.CreationTime -end $using:date).Days -gt 60 } |
+        Select-Object FullName,LastWriteTime,LastAccessTime,CreationTime |
+        Export-CSV $using:log\$using:folder-Drive-Audit.csv -NoTypeInformation -Append
+    }
+
+    return [pscustomobject]@{ ID=$job.Id; FOLDER=$folder }
+}
+
+"Starting script at $date" | Out-Host
+
+$childfolders = gci $rootfolder | Select-Object FullName,LastWriteTime,LastAccessTime,CreationTime,Name,Attributes
+
+ForEach ($childfolder in $childfolders) {
+    If ($childfolder.Attributes -like "*Archive*" -and (New-TimeSpan -start $childfolder.LastWriteTime -end $date).Days -gt 60 -and (New-TimeSpan -start $childfolder.CreationTime -end $date).Days -gt 60) {
+        $childfolder | Select-Object FullName,LastWriteTime,LastAccessTime,CreationTime |
+        Export-CSV $logfolder\Drive-Audit.csv -NoTypeInformation -Append
+    }
+    Elseif ($childfolder.Attributes -like "*Directory*") {
+        $wait = $TRUE
+        Do {
+            $waitcheck = @()
+            ForEach ($job in $jobs) {
+                $waitcheck += Get-Job -ID $job.ID
+            }
+            $runcount = ($waitcheck.State -match "Running").Count
+            If ($runcount -lt $MaxThreads -or $jobs -eq "") { $wait = $FALSE }
+            Else { cls; "$runcount threads reached, waiting for a job to complete before registering a new one..." | Out-Host; Start-Sleep 2 }
+        } while ($wait -eq $TRUE)
+        $jobs += MultiThread-Search -directory $childfolder -log $logfolder
+    }
+}
+
+Do {
+    $exit = 0
+    "Checking status of remaing jobs..." | Out-Host
+    ForEach ($job in $jobs) {
+        $id = $job.ID
+        $jobname = $job.FOLDER
+        $jobcheck = Get-Job -ID $id
+        $status = $jobcheck.State
+        If ($status -ne "Completed") { "Job #$id $jobname status $status..." | Out-Host }
+        ElseIf ($status -eq "Completed") { $exit++ }
+        ElseIf ($status -eq "Failed") { Write-Warning $jobcheck.ChildJobs[0].JobStateInfo.Reason.Message; $exit++ }
+    }
+    Start-Sleep 2
+    cls
+} while ($exit -lt $jobs.Count)
+
+$end = Get-Date
+$elapsed = ($end - $date)
+
+"Elapsed $elapsed" | Out-Host
+
+"Consolidating files at $end..." | Out-Host
+
+$files = (gci $logfolder -File).FullName | where { $_ -ne "$logfolder\Drive-Audit.csv" } | Sort-Object -Property FullName
+
+$filejob = Start-Job -ScriptBlock { 
+    ForEach ($file in $using:files) {
+        $import = Import-CSV $file
+        If ($import -ne $NULL) { $import | Export-CSV $using:logfolder\Drive-Audit.csv -NoTypeInformation -Append }
+    }
+}
+$filejobID = $filejob.ID
+
+"`nWriting to consolidated file in the background, please monitor the progress here: $logfolder\Drive-Audit.csv" | Out-Host
+Write-Host -ForegroundColor Green "Check progress via 'Get-Job -ID $filejobID'"
+
+$end = Get-Date
+$elapsed = ($end - $date)
+
+"`nTotal Elapsed time $elapsed" | Out-Host
---
+++ NFS-Client-Enable.ps1
@@ -0,0 +1,37 @@
+﻿Import-Module ActiveDirectory
+
+$computers = (Get-AdComputer -Filter * -SearchBase "OU=*,OU=*,DC=*,DC=*..." -SearchScope Subtree).Name
+$domain = "col-dev.ge.com"
+$failed = @()
+
+ForEach ($computer in $computers) {
+
+    If(!(Test-Connection -ComputerName $computer -Count 2 -Quiet -ErrorAction SilentlyContinue)) {
+        Write-Warning "Unable to connect to $computer, logging and moving on..."; $failed += [pscusomobject]@{ NAME=$computer; Reason="Offline"}; Continue
+    }
+    Else {
+        Copy-Item -Path "\\DC-1\c$\Windows\System32\WindowsPowerShell\v1.0\Modules\NFS" -Destination "\\$computer\c$\Windows\System32\WindowsPowerShell\v1.0\Modules\NFS" -force -Recurse
+        If (!(Test-Path -Path "\\$computer\c$\Windows\System32\WindowsPowerShell\v1.0\Modules\NFS" -ErrorAction SilentlyContinue)) {
+            Write-Warning "NFS module not found on $computer, logging and moving on..."; $failed += [pscustomobject]@{ NAME=$computer; Reason="Missing NFS Module"}; Continue }
+
+        Try {
+            Test-WSMan -ComputerName $computer -ErrorAction Stop | Out-Null
+        } Catch {
+            Write-Host -ForegroundColor Yellow "Enabling Winrm on $computer..."
+            $winrmenable = 'wmic /node:"' + $computer + '" process call create "cmd.exe /c winrm quickconfig -quiet"'
+            cmd.exe /c $winrmenable | Out-Null
+            If ((Invoke-Command -ComputerName $computer { 1 } -ErrorAction Ignore) -ne 1) { Write-Warning "Enabling Winrm failed on $computer, logging and moving on..."; $failed += [pscustomobject]@{ NAME=$computer; Reason="Unable to enable Winrm" }
+                Continue
+            }
+            Else { Write-Host -ForegroundColor Green "Successfully enabled Winrm on $computer, continuing..." }
+        } Finally {
+            Invoke-Command -ComputerName $computer -ScriptBlock {
+                Enable-WindowsOptionalFeature -FeatureName ServicesForNFS-ClientOnly,ClientForNFS-Infrastructure -Online -NoRestart -WarningAction Ignore | Out-Null
+                Import-Module NFS
+                Set-NFSMappingStore -EnableADLookup $TRUE -ADDomainName $domain | Out-Null
+            }
+        }
+    }
+}
+
+If ($failed -ne $NULL) { echo "`n`n"; Write-Warning "The following computers had errors:"; $failed }
